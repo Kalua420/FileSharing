ca48ba183a4ddf99ee3c0a4a830e3fd2b8d8a2ad
looser <crazykalua420@gmail.com>
2025-02-16 09:09:26 +0530

    16 Feb Commit

--- app/src/main/java/com/example/first/DatabaseHelper.java
+++ app/src/main/java/com/example/first/DatabaseHelper.java
@@ -24,7 +24,7 @@ public class DatabaseHelper {
     private static final String DB_NAME = "project";
     private static final String DB_USER = "root";
     private static final String DB_PASS = "root";
-    private static final String DB_IP = "192.168.107.137";
+    private static final String DB_IP = "192.168.75.137";
     private static final int DB_PORT = 3306;
     private static final int TIMEOUT = 5000;
 
